6840744817c0b86fd71fdb82f5d8039e6a6a0857
Subin Lee <chandar.sblee@gmail.com>
2022-02-08 17:16:50 +0900

    Python SDK 4.2.0:
    * 그룹 생성 시 SDK Version, OS Platform Version 표시하도록 수정

--- src/examples/modules/group/create_group.py
+++ src/examples/modules/group/create_group.py
@@ -2,5 +2,5 @@ import json
 from src.lib import message
 
 if __name__ == '__main__':
-    res = message.post('/messages/v4/groups', data={})
+    res = message.post('/messages/v4/groups', data=message.default_agent)
     print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
